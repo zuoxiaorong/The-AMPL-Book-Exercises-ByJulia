5696a4cb66b194bbc250b948079ee14fdcac3d63
左晓荣zuoxr <zuoxr@eav.top>
2023-03-08 17:38:23 +0800

    commit 15-1

--- Chapter-15-Network-Linear-Programs/15_1.jl
+++ Chapter-15-Network-Linear-Programs/15_1.jl
@@ -31,7 +31,35 @@ function Network_transshipment_problem_model(model, CITIES, Links, supply, deman
     return model
 end
 
-function  startmodel()
+function  newdata()
+    CITIES = ["A", "B", "C", "D", "E", "F"]
+    LINKS = [("A","B") ("A","C") ("B","C") ("B","D") ("B","E") ("C","E") ("D","E") ("D","F") ("E","F")]
+    C = 1:length(CITIES)
+    supply = [ifelse(CITIES[i] == "A", 100, ifelse(CITIES[i] == "B", 50, 0)) for i in C]
+    demand = [ifelse(CITIES[i] == "E", 30, ifelse(CITIES[i] == "F", 120, 0)) for i in C]
+    cost = [
+        0  13 12 0  0  0
+        0  0  1  6  9  0
+        0  0  0  0  7  0
+        0  0  0  0  4  13
+        0  0  0  0  0  12
+        0  0  0  0  0  0
+    ]
+    capacity = 80
+    return CITIES, LINKS, C, supply, demand, cost, capacity
+end
+
+function Network_transshipment_problem_newmodel(model, CITIES, supply, demand, cost, capacity)
+    @variable(model, Ship[i in CITIES, j in CITIES], lower_bound = 0, upper_bound = capacity)
+
+    @objective(model, Min, sum(cost[i,j]*Ship[i,j] for i in CITIES for j in CITIES))
+
+    @constraint(model, [i in CITIES, j in CITIES; cost[i,j] == 0], Ship[i,j] == 0)
+    @constraint(model, [i in CITIES], supply[i] + sum(Ship[j,i] for j in CITIES) == demand[i] + sum(Ship[i,j] for j in CITIES))
+    return model
+end
+
+function startmodel()
     optimizer = SCIP.Optimizer(display_verblevel=4 , limits_gap=0.00, parallel_maxnthreads=12)
     model = JuMP.direct_model(optimizer)
     set_time_limit_sec(model,100)
@@ -41,11 +69,23 @@ function  startmodel()
     optimize!(model)
     ship = value.(model[:Ship])
     assign_set = [(L[k] => ship[i,j]) for i in CITIES for j in CITIES for k in 1:length(LINKS) if  LINKS[k] == (i,j)]
-    println(assign_set)
 end
 
-startmodel()
+function startnewmodel()
+    optimizer = SCIP.Optimizer(display_verblevel=4 , limits_gap=0.00, parallel_maxnthreads=12)
+    model = JuMP.direct_model(optimizer)
+    set_time_limit_sec(model,100)
+    
+    C, L, CITIES, supply, demand, cost, capacity = newdata()
+    model = Network_transshipment_problem_newmodel(model, CITIES, supply, demand, cost, capacity)
+    optimize!(model)
+    ship = value.(model[:Ship])
+    assign_set = [((C[i], C[j]) => ship[i,j]) for i in CITIES for j in CITIES if ship[i,j] != 0]
+end
+startnewmodel()
 
 # Solution
-# (a)
+# (a) obj = +3.71000000000000e+03
 # [("A", "B") => 20.0, ("A", "C") => 80.0, ("B", "C") => 0.0, ("B", "D") => 70.0, ("B", "E") => 0.0, ("C", "E") => 80.0, ("D", "E") => 0.0, ("D", "F") => 70.0, ("E", "F") => 50.0]
+# (b) obj = +3.71000000000000e+03
+# [("A", "B") => 20.0, ("A", "C") => 80.0, ("B", "D") => 70.0, ("C", "E") => 80.0, ("D", "F") => 70.0, ("E", "F") => 50.0]
